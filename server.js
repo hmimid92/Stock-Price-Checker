07c8805b8e174f8faf4d03508744dc588ce2b9d7
HMIMID <ahmedhmimid177@gmail.com>
2025-03-15 17:50:13 +0000

    feat: adding security content middleware

--- server.js
+++ server.js
@@ -18,14 +18,13 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(helmet({
-  contentSecurityPolicy: {
-      directives: {
-        "script-src":["script.js","style.css"],
-      },
-      reportOnly:true
-    },
-}))
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+  }
+}));
 app.use(helmet());
 
 //Index page (static HTML)
